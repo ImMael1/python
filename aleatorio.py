ee5cbba18b52695bbf76f4495546928991ea7834
ImMael1 <kadu.m.pantoja003@gmail.com>
2024-02-02 10:00:58 -0300

    py

---
+++ aleatorio.py
@@ -0,0 +1,11 @@
+import random
+
+opcoes_usuario = input("Insira as opcoes a serem sorteadas, separados por vírgula:")
+
+opcoes = opcoes_usuario.split(',')
+
+opcoes = [opcao.strip() for opcao in opcoes]
+
+escolha_aleatoria = random.choice(opcoes)
+
+print("A escolha aleatória é:", escolha_aleatoria)
